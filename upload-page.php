78279ae3809e092e2c432ba3e10e772e04f71ee1
ebalczewski <balczewskie@carleton.edu>
2015-06-29 17:45:01 -0500

    Removed useless files

--- upload-page.php
+++
@@ -1,7 +0,0 @@
-if(count($_FILES['uploads']['filesToUpload'])) {
-    foreach ($_FILES['uploads']['filesToUpload'] as $file) {
-        
-        //do your upload stuff here
-        echo $file;
-    }
-}
